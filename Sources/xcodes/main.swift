afc84200d9bd04573fd754296487f2d3a9867e86
Ruslan Alikhamov <r.alikhamov@gmail.com>
2020-11-23 23:18:39 +0300

    refactoring

--- Sources/xcodes/main.swift
+++ Sources/xcodes/main.swift
@@ -20,9 +20,62 @@ migrateApplicationSupportFiles()
 var app: Command!
 app = Command(usage: "xcodes") { _, _ in print(GuakaConfig.helpGenerator.init(command: app).helpMessage) }
 
-let installed = Command(usage: "installed",
-                        shortMessage: "List the versions of Xcode that are installed") { _, _ in
-    installer.printInstalledXcodes()
+func installed() -> Command {
+    let installed = Command(usage: "installed",
+                            shortMessage: "List the versions of Xcode that are installed") { _, _ in
+        installer.printInstalledXcodes()
+            .done {
+                exit(0)
+            }
+            .catch { error in
+                print(error.legibleLocalizedDescription)
+                exit(1)
+            }
+        
+        RunLoop.current.run()
+    }
+    return installed
+}
+
+func select() -> Command {
+    let printFlag = Flag(shortName: "p", longName: "print-path", value: false, description: "Print the path of the selected Xcode")
+    return Command(usage: "select <version or path>",
+                   shortMessage: "Change the selected Xcode",
+                   longMessage: "Change the selected Xcode. Run without any arguments to interactively select from a list, or provide an absolute path.",
+                   flags: [printFlag],
+                   example: """
+                                  xcodes select
+                                  xcodes select 11.4.0
+                                  xcodes select /Applications/Xcode-11.4.0.app
+                                  xcodes select -p
+                                  """) { flags, args in
+        selectXcode(shouldPrint: flags.getBool(name: "print-path") ?? false, pathOrVersion: args.joined(separator: " "))
+            .catch { error in
+                print(error.legibleLocalizedDescription)
+                exit(1)
+            }
+        
+        RunLoop.current.run()
+    }
+}
+
+let showDateFlag = Flag(longName: "print-dates", value: false, description: "Print release dates for each version")
+func list() -> Command {
+    return Command(usage: "list",
+                   shortMessage: "List all versions of Xcode that are available to install",
+                   flags: [showDateFlag]) { flags, _ in
+        firstly { () -> Promise<Void> in
+            var shouldPrintDates = false
+            if flags.getBool(name: "print-dates") == true {
+                shouldPrintDates = true
+            }
+            if xcodeList.shouldUpdate {
+                return installer.updateAndPrint(shouldPrintDates: shouldPrintDates)
+            }
+            else {
+                return installer.printAvailableXcodes(xcodeList.availableXcodes, installed: Current.files.installedXcodes(), shouldPrintDates: shouldPrintDates)
+            }
+        }
         .done {
             exit(0)
         }
@@ -30,78 +83,30 @@ let installed = Command(usage: "installed",
             print(error.legibleLocalizedDescription)
             exit(1)
         }
-
-    RunLoop.current.run()
+        
+        RunLoop.current.run()
+    }
 }
-app.add(subCommand: installed)
 
-let printFlag = Flag(shortName: "p", longName: "print-path", value: false, description: "Print the path of the selected Xcode")
-let select = Command(usage: "select <version or path>",
-                     shortMessage: "Change the selected Xcode",
-                     longMessage: "Change the selected Xcode. Run without any arguments to interactively select from a list, or provide an absolute path.",
-                     flags: [printFlag],
-                     example: """
-                              xcodes select
-                              xcodes select 11.4.0
-                              xcodes select /Applications/Xcode-11.4.0.app
-                              xcodes select -p
-                              """) { flags, args in
-    selectXcode(shouldPrint: flags.getBool(name: "print-path") ?? false, pathOrVersion: args.joined(separator: " "))
+func update() -> Command {
+    return Command(usage: "update",
+                   shortMessage: "Update the list of available versions of Xcode",
+                   flags: [showDateFlag]) { flags, _ in
+        firstly { () -> Promise<Void> in
+            var shouldPrintDates = false
+            if flags.getBool(name: "print-dates") == true {
+                shouldPrintDates = true
+            }
+            return installer.updateAndPrint(shouldPrintDates: shouldPrintDates)
+        }
         .catch { error in
             print(error.legibleLocalizedDescription)
             exit(1)
         }
-
-    RunLoop.current.run()
+        
+        RunLoop.current.run()
+    }
 }
-app.add(subCommand: select)
-
-let showDateFlag = Flag(longName: "print-dates", value: false, description: "Print release dates for each version")
-let list = Command(usage: "list",
-                   shortMessage: "List all versions of Xcode that are available to install",
-                   flags: [showDateFlag]) { flags, _ in
-    firstly { () -> Promise<Void> in
-        var shouldPrintDates = false
-        if flags.getBool(name: "print-dates") == true {
-            shouldPrintDates = true
-        }
-        if xcodeList.shouldUpdate {
-            return installer.updateAndPrint(shouldPrintDates: shouldPrintDates)
-        }
-        else {
-            return installer.printAvailableXcodes(xcodeList.availableXcodes, installed: Current.files.installedXcodes(), shouldPrintDates: shouldPrintDates)
-        }
-    }
-    .done {
-        exit(0)
-    }
-    .catch { error in
-        print(error.legibleLocalizedDescription)
-        exit(1)
-    }
-
-    RunLoop.current.run()
-}
-app.add(subCommand: list)
-
-let update = Command(usage: "update",
-                     shortMessage: "Update the list of available versions of Xcode",
-                     flags: [showDateFlag]) { flags, _ in
-    firstly { () -> Promise<Void> in
-        var shouldPrintDates = false
-        if flags.getBool(name: "print-dates") == true {
-            shouldPrintDates = true
-        }
-        return installer.updateAndPrint(shouldPrintDates: shouldPrintDates)
-    }
-    .catch { error in
-        print(error.legibleLocalizedDescription)
-        exit(1)
-    }
-
-    RunLoop.current.run()
-}
-app.add(subCommand: update)
 
 func downloadCommand(shouldInstall: Bool) -> Command {
     let pathFlag = Flag(longName: "path", type: String.self, description: "Local path to Xcode .xip")
@@ -112,14 +117,14 @@ func downloadCommand(shouldInstall: Bool) -> Command {
     let commandName = shouldInstall ? "install" : "download"
     let commandInstruction = shouldInstall ? "Download and install" : "Download"
     return Command(usage: "\(commandName) <version>",
-                          shortMessage: "Download and install a specific version of Xcode",
-                          longMessage: """
+                   shortMessage: "Download and install a specific version of Xcode",
+                   longMessage: """
                           \(commandInstruction) a specific version of Xcode
 
                           By default, xcodes will use a URLSession to download the specified version. If aria2 (https://aria2.github.io, available in Homebrew) is installed, either at /usr/local/bin/aria2c or at the path specified by the --aria2 flag, then it will be used instead. aria2 will use up to 16 connections to download Xcode 3-5x faster. If you have aria2 installed and would prefer to not use it, you can use the --no-aria2 flag.
                           """,
-                          flags: [pathFlag, latestFlag, latestPrereleaseFlag, aria2, noAria2],
-                          example: """
+                   flags: [pathFlag, latestFlag, latestPrereleaseFlag, aria2, noAria2],
+                   example: """
                                    xcodes \(commandName) 10.2.1
                                    xcodes \(commandName) 11 Beta 7
                                    xcodes \(commandName) 11.2 GM seed
@@ -127,7 +132,7 @@ func downloadCommand(shouldInstall: Bool) -> Command {
                                    xcodes \(commandName) --latest-prerelease
                                    """) { flags, args in
         let versionString = args.joined(separator: " ")
-
+        
         let installation: XcodeInstaller.InstallationType
         if flags.getBool(name: "latest") == true {
             installation = .latest
@@ -144,51 +149,59 @@ func downloadCommand(shouldInstall: Bool) -> Command {
         if aria2Path.exists, flags.getBool(name: "no-aria2") != true {
             downloader = .aria2(aria2Path)
         }
-
+        
         installer.install(installation, downloader: downloader, shouldInstall: shouldInstall)
             .catch { error in
                 switch error {
-                case Process.PMKError.execution(let process, let standardOutput, let standardError):
-                    Current.logging.log("""
+                    case Process.PMKError.execution(let process, let standardOutput, let standardError):
+                        Current.logging.log("""
                         Failed executing: `\(process)` (\(process.terminationStatus))
                         \([standardOutput, standardError].compactMap { $0 }.joined(separator: "\n"))
                         """)
-                default:
-                    Current.logging.log(error.legibleLocalizedDescription)
+                    default:
+                        Current.logging.log(error.legibleLocalizedDescription)
                 }
-
+                
                 exit(1)
             }
-
+        
         RunLoop.current.run()
     }
 }
 
-let install = downloadCommand(shouldInstall: true)
-app.add(subCommand: install)
-
-let download = downloadCommand(shouldInstall: false)
-app.add(subCommand: download)
-
-let uninstall = Command(usage: "uninstall <version>",
-                        shortMessage: "Uninstall a specific version of Xcode",
-                        example: "xcodes uninstall 10.2.1") { _, args in
+func uninstall() -> Command {
+    Command(usage: "uninstall <version>",
+            shortMessage: "Uninstall a specific version of Xcode",
+            example: "xcodes uninstall 10.2.1") { _, args in
         let versionString = args.joined(separator: " ")
-    installer.uninstallXcode(versionString)
-        .catch { error in
-            print(error.legibleLocalizedDescription)
-            exit(1)
-        }
-
-    RunLoop.current.run()
+        installer.uninstallXcode(versionString)
+            .catch { error in
+                print(error.legibleLocalizedDescription)
+                exit(1)
+            }
+        
+        RunLoop.current.run()
+    }
 }
-app.add(subCommand: uninstall)
 
-let version = Command(usage: "version",
-                      shortMessage: "Print the version number of xcodes itself") { _, _ in
-    print(XcodesKit.version)
-    exit(0)
+func version() -> Command {
+    Command(usage: "version",
+            shortMessage: "Print the version number of xcodes itself") { _, _ in
+        print(XcodesKit.version)
+        exit(0)
+    }
 }
-app.add(subCommand: version)
+
+
+func setupCommands() {
+    app.add(subCommand: installed())
+    app.add(subCommand: select())
+    app.add(subCommand: list())
+    app.add(subCommand: update())
+    app.add(subCommand: downloadCommand(shouldInstall: true))
+    app.add(subCommand: downloadCommand(shouldInstall: false))
+    app.add(subCommand: version())
+    app.add(subCommand: uninstall())
+}
 
 app.execute()
